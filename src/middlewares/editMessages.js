069c9b45807a4f01a000f83cbf4cf94a2175fa43
pullso <pullso.code@gmail.com>
2021-11-16 14:58:35 +0300

    fix delete

--- src/middlewares/editMessages.js
+++ src/middlewares/editMessages.js
@@ -3,10 +3,10 @@ import _ from "lodash";
 export default function (ctx, next) {
   const messages = _.get(ctx,'session.deleteMessageIds', [])
   if (messages.length) {
-    _.each(messages, (async (m, index) => {
+    _.each(messages, (async ({chat_id, message_id}, index) => {
       try {
-        await ctx.telegram.deleteMessage(m.chat_id, m.message_id)
-        delete ctx.session.deleteMessageIds[index]
+        await ctx.telegram.deleteMessage(chat_id, message_id)
+        _.remove(messages, {message_id})
       } catch (e) {
         console.log(e)
       }
