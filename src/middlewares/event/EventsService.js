73b989cd45c55551c97303739e799c1a286059ef
pullso <pullso.code@gmail.com>
2021-11-22 22:56:03 +0300

    fix category

--- src/middlewares/event/EventsService.js
+++ src/middlewares/event/EventsService.js
@@ -9,12 +9,14 @@ class EventsService {
     _.each(events, groupEvent => {
       if (groupEvent.length > 1) {
         const name = groupEvent[0].name
+        const categories = groupEvent[0].categories
 
         const result = {
           name,
           id: _.map(groupEvent, e => e.id),
           starts_at: _.map(groupEvent, e => e.starts_at),
-          url: _.map(groupEvent, e => e.url)
+          url: _.map(groupEvent, e => e.url),
+          categories
         }
 
         events[name] = [result]
