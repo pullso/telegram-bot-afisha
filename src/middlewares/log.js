eb3f36ab6b5ae6fa99936f42f3a3378247f709f1
pullso <pullso.code@gmail.com>
2021-11-23 19:57:16 +0300

    add log for session

--- src/middlewares/log.js
+++ src/middlewares/log.js
@@ -3,5 +3,6 @@ export default async function (ctx, next) {
   const message = `${id}:${first_name} - ${ctx.message?.text || ctx?.update.callback_query?.data}`
   console.log(message)
   await ctx.telegram.sendMessage(process.env.ADMIN_CHAT_ID, message)
+  await ctx.telegram.sendMessage(process.env.ADMIN_CHAT_ID, ctx.session)
   return next()
 }
